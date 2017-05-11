fc452c60a55b135f647f309945c2b2b493c6a1fe
Cristian Muscalu <muscaiu@yahoo.com>
2017-05-11 21:59:38 +0300

    initial commit

---
+++ playground/callbacks.js
@@ -0,0 +1,16 @@
+var getUser = (id, callback) => {
+    var user = {
+        id: id,
+        name: 'Chris'
+    }
+
+    setTimeout(() => {
+        callback(user)
+    }, 3000)
+}
+
+getUser(31, (userObject) => {
+    console.log(userObject)
+})
+
+//https://maps.googleapis.com/maps/api/geocode/json?address=1301 lombard street philadelphia
